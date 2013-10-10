c2cdacaf5701dd271ec8735f104e7018f6bb1b52
Will Ross <paxswill@gmail.com>
2013-10-10 12:16:05 -0400

    Fix a few places the old domain was left
    Also fixed a duplication bug with feeds

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://github.paxswill.com'
+SITEURL = 'http://paxswill.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
